6b9ff8ec92ffb689b0b7e529ba9a3a64bcbe1455
rpapa7619 <71879068+rpapa7619@users.noreply.github.com>
2020-10-26 15:44:00 +0800

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# this script is only tested on ubuntu xenial
+# this script is only tested on ubuntu bionic
 
 # install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ubuntu
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
 
 # run jenkins
 mkdir -p /var/jenkins_home
@@ -22,3 +26,4 @@ docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+
